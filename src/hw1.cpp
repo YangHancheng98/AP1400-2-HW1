9d532112470ea9325dd570581d5f8d01cf2007b3
desktopYhc <yanghancheng7@gmail.com>
2025-07-20 03:09:22 +0800

    only 5 tests left!!!

--- src/hw1.cpp
+++ src/hw1.cpp
@@ -1,1 +1,245 @@
 #include "hw1.h"
+
+#include <cmath>
+#include <iomanip>
+#include <iostream>
+#include <random>
+
+namespace algebra {
+Matrix zeros(size_t n, size_t m) {
+    Matrix matrix(n, std::vector<double>(m, 0));
+    return matrix;
+}
+
+Matrix ones(size_t n, size_t m) {
+    Matrix matrix(n, std::vector<double>(m, 1));
+    return matrix;
+}
+
+Matrix random(size_t n, size_t m, double min, double max) {
+    if (min >= max) {
+        throw std::logic_error("Min value cannot be greater than max value.");
+    }
+    std::random_device rd;
+    std::mt19937 mt(rd());
+    std::uniform_real_distribution<double> dist(
+        min, std::nextafter(max, __DBL_MAX__));
+
+    Matrix randomMatrix = zeros(n, m);
+    for (auto& row : randomMatrix) {
+        for (auto& elem : row) {
+            elem = dist(mt);
+        }
+    }
+
+    return randomMatrix;
+}
+
+void show(const Matrix& matrix) {
+    for (const auto& row : matrix) {
+        for (const auto& elem : row) {
+            std::cout << std::setw(10) << std::setprecision(3) << elem << " ";
+        }
+        std::cout << endl;
+    }
+}
+
+Matrix multiply(const Matrix& matrix, double c) {
+    Matrix resultMatrix = zeros(matrix.size(), matrix[0].size());
+    for (int col = 0; col < matrix[0].size(); col++) {
+        for (int row = 0; row < matrix.size(); row++) {
+            resultMatrix[row][col] = c * matrix[row][col];
+        }
+    }
+    return resultMatrix;
+}
+
+Matrix multiply(const Matrix& matrix1, const Matrix& matrix2) {
+    if (matrix1.size() == 0 || matrix2.size() == 0) {
+        return Matrix{};
+    }
+    if (matrix1[0].size() != matrix2.size()) {
+        throw std::logic_error("Invalid matrix format for multiplication.");
+    }
+
+    int resultRow = matrix2[0].size();
+    int resultCol = matrix1.size();
+    Matrix resultMatrix = zeros(resultCol, resultRow);
+    for (int j = 0; j < resultRow; j++) {
+        for (int i = 0; i < resultCol; i++) {
+            double sum = 0;
+            for (int m = 0; m < matrix1[0].size(); m++) {
+                sum += matrix1[i][m] * matrix2[m][j];
+            }
+            resultMatrix[i][j] = sum;
+        }
+    }
+    return resultMatrix;
+}
+
+Matrix sum(const Matrix& matrix, double c) {
+    if (matrix.size() == 0) {
+        return Matrix{};
+    }
+    Matrix resultMatrix = zeros(matrix.size(), matrix[0].size());
+    for (int col = 0; col < matrix[0].size(); col++) {
+        for (int row = 0; row < matrix.size(); row++) {
+            resultMatrix[row][col] = c + matrix[row][col];
+        }
+    }
+    return resultMatrix;
+}
+
+Matrix sum(const Matrix& matrix1, const Matrix& matrix2) {
+    if (matrix1.size() != matrix2.size()) {
+        throw std::logic_error("Invalid matrix format for sum.");
+    }
+    if (matrix1.size() == 0 || matrix2.size() == 0) {
+        return Matrix{};
+    }
+    if (matrix1[0].size() != matrix2[0].size()) {
+        throw std::logic_error("Invalid matrix format for sum.");
+    }
+
+    Matrix resultMatrix = zeros(matrix1.size(), matrix1[0].size());
+    for (int col = 0; col < matrix1[0].size(); col++) {
+        for (int row = 0; row < matrix1.size(); row++) {
+            resultMatrix[row][col] = matrix1[row][col] + matrix2[row][col];
+        }
+    }
+    return resultMatrix;
+}
+
+// From here, the variable naming is correct
+Matrix transpose(const Matrix& matrix) {
+    if (matrix.size() == 0) {
+        return Matrix{};
+    }
+    int originRow = matrix[0].size();
+    int originCol = matrix.size();
+    Matrix resultMatrix = zeros(originRow, originCol);
+    for (int row = 0; row < originRow; row++) {
+        for (int col = 0; col < originCol; col++) {
+            resultMatrix[row][col] = matrix[col][row];
+        }
+    }
+    return resultMatrix;
+}
+
+Matrix minor(const Matrix& matrix, size_t rowToRemove, size_t colToRemove) {
+    if (matrix.size() == 0 || matrix[0].size() == 0) {
+        throw std::logic_error("Invalid matrix format for minor.");
+    }
+    if (rowToRemove >= matrix[0].size() || colToRemove >= matrix.size()) {
+        throw std::logic_error("Invalid row/column index for minor.");
+    }
+    size_t n = matrix.size();
+    size_t m = matrix[0].size();
+    Matrix result;
+
+    for (size_t i = 0; i < n; ++i) {
+        if (i == rowToRemove) continue;
+        std::vector<double> row;
+        for (size_t j = 0; j < m; ++j) {
+            if (j == colToRemove) continue;
+            row.push_back(matrix[i][j]);
+        }
+        result.push_back(row);
+    }
+    return result;
+}
+
+double determinant(const Matrix& matrix) {
+    if (matrix.size() == 0 || matrix[0].size() == 0) {
+        return 1;
+    }
+    if (matrix.size() != matrix[0].size()) {
+        throw std::logic_error("Determinant should apply on square matrix.");
+    }
+    if (matrix.size() == 2) {
+        double a = matrix[0][0];
+        double b = matrix[0][1];
+        double c = matrix[1][0];
+        double d = matrix[1][1];
+        return a * d - b * c;
+    }
+    double result = 0;
+    for (int col = 0; col < matrix[0].size(); col++) {
+        double sign = (col % 2 == 0) ? 1 : -1;
+        result +=
+            pow(-1, col) * matrix[0][col] * determinant(minor(matrix, 0, col));
+    }
+    return result;
+}
+
+namespace {
+Matrix getCofactorMatrix(const Matrix& matrix) {
+    int row = matrix.size();
+    int col = matrix[0].size();
+    Matrix resultMatrix = zeros(row, col);
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < col; j++) {
+            resultMatrix[i][j] =
+                pow(-1, i + j) * determinant(minor(matrix, i, j));
+        }
+    }
+    return resultMatrix;
+}
+
+Matrix getAdjMatrix(const Matrix& matrix) {
+    return transpose(getCofactorMatrix(matrix));
+}
+}  // namespace
+
+Matrix inverse(const Matrix& matrix) {
+    if (matrix.size() == 0) {
+        return Matrix{};
+    }
+    if (matrix.size() != matrix[0].size()) {
+        throw std::logic_error("inverse should apply on square matrix.");
+    }
+    double det = determinant(matrix);
+    if (det == 0) {
+        throw std::logic_error("singular matrices have no inverse.");
+    }
+
+    return multiply(getAdjMatrix(matrix), 1.0 / det);
+}
+
+Matrix concatenate(const Matrix& matrix1, const Matrix& matrix2, int axis = 0) {
+    int row1 = matrix1.size();
+    int row2 = matrix2.size();
+    int col1 = matrix1[0].size();
+    int col2 = matrix2[0].size();
+
+    if (axis == 0 && col1 != col2) {
+        throw std::logic_error(
+            "concatenate vertically, the column should be equal.");
+    }
+    if (axis == 1 && row1 != row2) {
+        throw std::logic_error(
+            "concatenate horizontally, the column should be equal.");
+    }
+    Matrix result;
+    if (axis == 0) {
+        result = matrix1;
+        result.insert(result.end(), matrix2.begin(), matrix2.end());
+        return result;
+    } else if (axis == 1) {
+        result = matrix1;
+        for (int i = 0; i < result.size(); i++) {
+            result[i].insert(result[i].end(), matrix2[i].begin(),
+                             matrix2[i].end());
+        }
+        return result;
+    } else {
+        throw std::logic_error(
+            "Invalid axis. Use 0 (vertical) or 1 (horizontal).");
+    }
+}
+
+// Matrix ero_swap(const Matrix& matrix, size_t r1, size_t r2);
+// Matrix ero_multiply(const Matrix& matrix, size_t r, double c);
+// Matrix ero_sum(const Matrix& matrix, size_t r1, double c, size_t r2);
+// Matrix upper_triangular(const Matrix& matrix);
+}  // namespace algebra
--- src/unit_test.cpp
+++ src/unit_test.cpp
@@ -1,9 +1,8 @@
 
-#include "gtest/gtest.h"
 #include "gmock/gmock.h"
+#include "gtest/gtest.h"
 #include "hw1.h"
 
-/*
 TEST(HW1Test, ZEROS) {
     Matrix matrix{algebra::zeros(5, 6)};
 
@@ -13,8 +12,7 @@ TEST(HW1Test, ZEROS) {
 
     // check the value of the elements
     for (const auto& row : matrix)
-        for (const auto& elem : row)
-            EXPECT_DOUBLE_EQ(elem, 0);
+        for (const auto& elem : row) EXPECT_DOUBLE_EQ(elem, 0);
 }
 
 TEST(HW1Test, ONES) {
@@ -26,8 +24,7 @@ TEST(HW1Test, ONES) {
 
     // check the value of the elements
     for (const auto& row : matrix)
-        for (const auto& elem : row)
-            EXPECT_DOUBLE_EQ(elem, 1);
+        for (const auto& elem : row) EXPECT_DOUBLE_EQ(elem, 1);
 }
 
 TEST(HW1Test, RANDOM1) {
@@ -43,8 +40,7 @@ TEST(HW1Test, RANDOM1) {
 
     // check the value of the elements
     for (const auto& row : matrix)
-        for (const auto& elem : row)
-            EXPECT_THAT(elem, AllOf(Ge(-5.0), Lt(7)));
+        for (const auto& elem : row) EXPECT_THAT(elem, AllOf(Ge(-5.0), Lt(7)));
 
     // show the matrix to test algebra::show function
     std::cout << "random matrix [-5, 7)" << std::endl;
@@ -68,7 +64,7 @@ TEST(HW1Test, MULTIPLY1) {
     // check the value of the elements
     for (size_t i{}; i < mult.size(); i++)
         for (size_t j{}; j < mult[i].size(); j++)
-            EXPECT_NEAR(mult[i][j], matrix[i][j]*3.5, 0.03);
+            EXPECT_NEAR(mult[i][j], matrix[i][j] * 3.5, 0.03);
 }
 
 TEST(HW1Test, MULTIPLY2) {
@@ -77,12 +73,15 @@ TEST(HW1Test, MULTIPLY2) {
     EXPECT_TRUE(matrix.empty());
 
     // Caution: matrices with wrong dimensions cannot be multiplied
-    EXPECT_THROW(algebra::multiply(Matrix{{1, 2, 3}, {4, 5, 6}}, Matrix{{1, 2, 3}, {4, 5, 6}}), std::logic_error);
+    EXPECT_THROW(algebra::multiply(Matrix{{1, 2, 3}, {4, 5, 6}},
+                                   Matrix{{1, 2, 3}, {4, 5, 6}}),
+                 std::logic_error);
 }
 
 TEST(HW1Test, MULTIPLY3) {
     Matrix matrix1{{-3, 3, 1.7}, {4, -4, 2.6}, {-5, 5, 3.5}};
     Matrix matrix2{{2.5}, {-2}, {-5.5}};
+
     Matrix matrix{algebra::multiply(matrix1, matrix2)};
 
     // check the size of the matrix
@@ -124,13 +123,12 @@ TEST(HW1Test, SUM1) {
     // check the value of the elements
     for (size_t i{}; i < sum.size(); i++)
         for (size_t j{}; j < sum[i].size(); j++)
-            EXPECT_NEAR(sum[i][j], matrix[i][j]+2.44, 0.03);
+            EXPECT_NEAR(sum[i][j], matrix[i][j] + 2.44, 0.03);
 }
 
 TEST(HW1Test, SUM2) {
     // Caution: sum of 2 empty matrices
     EXPECT_TRUE(algebra::sum(Matrix{}, Matrix{}).empty());
-
     // Caution: matrices with wrong dimensions cannot be summed
     EXPECT_THROW(algebra::sum(Matrix{{1, 2, 3}}, Matrix{}), std::logic_error);
 
@@ -145,7 +143,7 @@ TEST(HW1Test, SUM2) {
     // check the value of the elements
     for (size_t i{}; i < sum.size(); i++)
         for (size_t j{}; j < sum[i].size(); j++)
-            EXPECT_NEAR(sum[i][j], matrix1[i][j]+matrix2[i][j], 0.03);
+            EXPECT_NEAR(sum[i][j], matrix1[i][j] + matrix2[i][j], 0.03);
 }
 
 TEST(HW1Test, TRANSPOSE) {
@@ -200,16 +198,23 @@ TEST(HW1Test, DETERMINANT1) {
     EXPECT_EQ(algebra::determinant(Matrix{}), 1);
 
     // Caution: non-square matrices have no determinant
-    EXPECT_THROW(algebra::determinant(Matrix{{1, 2, 3}, {4, 5, 6}}), std::logic_error);
+    EXPECT_THROW(algebra::determinant(Matrix{{1, 2, 3}, {4, 5, 6}}),
+                 std::logic_error);
 
     // test case
-    Matrix matrix{{-1, 1.5, -1.75, -2}, {-2, 2.5, -2.75, -3}, {3, 3.5, -3.75, -4}, {4, 4.5, 4.75, -5}};
+    Matrix matrix{{-1, 1.5, -1.75, -2},
+                  {-2, 2.5, -2.75, -3},
+                  {3, 3.5, -3.75, -4},
+                  {4, 4.5, 4.75, -5}};
     EXPECT_NEAR(algebra::determinant(matrix), -28.5, 0.03);
 }
 
 TEST(HW1Test, DETERMINANT2) {
     // test case
-    Matrix matrix{{1, 1.5, -1.75, 2}, {2, 2.5, -2.75, 3}, {3, 3.5, -3.75, 4}, {4, 4.5, 4.75, 5}};
+    Matrix matrix{{1, 1.5, -1.75, 2},
+                  {2, 2.5, -2.75, 3},
+                  {3, 3.5, -3.75, 4},
+                  {4, 4.5, 4.75, 5}};
     EXPECT_DOUBLE_EQ(algebra::determinant(matrix), 0);
 }
 
@@ -218,14 +223,21 @@ TEST(HW1Test, INVERSE1) {
     EXPECT_TRUE(algebra::inverse(Matrix{}).empty());
 
     // Caution: non-square matrices have no inverse
-    EXPECT_THROW(algebra::inverse(Matrix{{1, 2, 3}, {40, 5, -6}}), std::logic_error);
+    EXPECT_THROW(algebra::inverse(Matrix{{1, 2, 3}, {40, 5, -6}}),
+                 std::logic_error);
 
     // Caution: singular matrices have no inverse
-    Matrix non_singular_matrix{{1, 1.5, -1.75, 2}, {2, 2.5, -2.75, 3}, {3, 3.5, -3.75, 4}, {4, 4.5, 4.75, 5}};
+    Matrix non_singular_matrix{{1, 1.5, -1.75, 2},
+                               {2, 2.5, -2.75, 3},
+                               {3, 3.5, -3.75, 4},
+                               {4, 4.5, 4.75, 5}};
     EXPECT_THROW(algebra::inverse(non_singular_matrix), std::logic_error);
 
     // test case
-    Matrix matrix{{-1, 1.5, -1.75, -2}, {-2, 2.5, -2.75, -3}, {3, 3.5, -3.75, -4}, {4, 4.5, 4.75, -5}};
+    Matrix matrix{{-1, 1.5, -1.75, -2},
+                  {-2, 2.5, -2.75, -3},
+                  {3, 3.5, -3.75, -4},
+                  {4, 4.5, 4.75, -5}};
     Matrix inverse{algebra::inverse(matrix)};
     EXPECT_NEAR(inverse[0][0], 0.16, 0.03);
     EXPECT_NEAR(inverse[1][1], 3.31, 0.03);
@@ -233,10 +245,12 @@ TEST(HW1Test, INVERSE1) {
     EXPECT_NEAR(inverse[0][3], 0, 0.03);
 }
 
-
 TEST(HW1Test, INVERSE2) {
     // test case
-    Matrix matrix{{-12, 1.5, -1.75, -2}, {-2, 2.5, -2.75, -3}, {25, 3.5, -3.75, -4}, {4, 4.5, 4.75, -51}};
+    Matrix matrix{{-12, 1.5, -1.75, -2},
+                  {-2, 2.5, -2.75, -3},
+                  {25, 3.5, -3.75, -4},
+                  {4, 4.5, 4.75, -51}};
     Matrix inverse{algebra::inverse(matrix)};
     EXPECT_NEAR(inverse[0][0], 0.05, 0.03);
     EXPECT_NEAR(inverse[1][1], 18.07, 0.03);
@@ -246,7 +260,9 @@ TEST(HW1Test, INVERSE2) {
 
 TEST(HW1Test, CONCATENATE1) {
     // Caution: matrices with wrong dimensions cannot be concatenated
-    EXPECT_THROW(algebra::concatenate(Matrix{{1, 2}}, Matrix{{1, 2, 3}, {4, 5, 6}}, 0), std::logic_error);
+    EXPECT_THROW(
+        algebra::concatenate(Matrix{{1, 2}}, Matrix{{1, 2, 3}, {4, 5, 6}}, 0),
+        std::logic_error);
 
     // test case
     Matrix matrix1{algebra::random(2, 3, 0, 1)};
@@ -262,12 +278,13 @@ TEST(HW1Test, CONCATENATE1) {
     EXPECT_DOUBLE_EQ(matrix[1][2], matrix1[1][2]);
     EXPECT_DOUBLE_EQ(matrix[2][0], matrix2[0][0]);
     EXPECT_DOUBLE_EQ(matrix[5][2], matrix2[3][2]);
-
 }
 
 TEST(HW1Test, CONCATENATE2) {
     // Caution: matrices with wrong dimensions cannot be concatenated
-    EXPECT_THROW(algebra::concatenate(Matrix{{1, 2}}, Matrix{{1, 2, 3}, {4, 5, 6}}, 1), std::logic_error);
+    EXPECT_THROW(
+        algebra::concatenate(Matrix{{1, 2}}, Matrix{{1, 2, 3}, {4, 5, 6}}, 1),
+        std::logic_error);
 
     // test case
     Matrix matrix1{algebra::random(2, 3, 0, 1)};
@@ -284,7 +301,7 @@ TEST(HW1Test, CONCATENATE2) {
     EXPECT_DOUBLE_EQ(matrix[0][3], matrix2[0][0]);
     EXPECT_DOUBLE_EQ(matrix[1][3], matrix2[1][0]);
 }
-
+/*
 TEST(HW1Test, ERO_SWAP) {
     // Caution: r1 or r2 inputs are out of range
     EXPECT_THROW(algebra::ero_swap(Matrix{{1, 2}}, 0, 1), std::logic_error);
@@ -310,7 +327,7 @@ TEST(HW1Test, ERO_MULTIPLY) {
 
     // check the value of the elements
     for (size_t i{}; i < ero[2].size(); i++)
-        EXPECT_NEAR(ero[2][i], matrix[2][i]*1.5, 0.03);
+        EXPECT_NEAR(ero[2][i], matrix[2][i] * 1.5, 0.03);
 }
 
 TEST(HW1Test, ERO_SUM) {
@@ -323,7 +340,7 @@ TEST(HW1Test, ERO_SUM) {
 
     // check the value of the elements
     for (size_t i{}; i < ero[3].size(); i++)
-        EXPECT_NEAR(ero[3][i], matrix[0][i]*2+matrix[3][i], 0.03);
+        EXPECT_NEAR(ero[3][i], matrix[0][i] * 2 + matrix[3][i], 0.03);
 }
 
 TEST(HW1Test, UPPER_TRIANGULAR1) {
@@ -331,7 +348,8 @@ TEST(HW1Test, UPPER_TRIANGULAR1) {
     EXPECT_TRUE(algebra::upper_triangular(Matrix{}).empty());
 
     // Caution: non-square matrices have no upper triangular form
-    EXPECT_THROW(algebra::upper_triangular(Matrix{{1, 2, 3}, {4, 5, 6}}), std::logic_error);
+    EXPECT_THROW(algebra::upper_triangular(Matrix{{1, 2, 3}, {4, 5, 6}}),
+                 std::logic_error);
 
     // test case 1
     Matrix matrix1{{1, 2}, {5, 7}};
@@ -358,9 +376,6 @@ TEST(HW1Test, BONUS) {
     EXPECT_NEAR(res2[1][0], 0, 0.03);
     EXPECT_NEAR(res2[2][0], 0, 0.03);
     EXPECT_NEAR(res2[2][1], 0, 0.03);
-    EXPECT_NEAR(res2[2][2], 39/4.0, 0.03);
+    EXPECT_NEAR(res2[2][2], 39 / 4.0, 0.03);
 }
 */
-
-
-
